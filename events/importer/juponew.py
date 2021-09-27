534448fd2292d2e37f692129d6dd0b48df0ae7bc
Anthon98 <anthon.kallankari@gmail.com>
2021-09-27 14:09:33 +0300

    Type checking, small logic improvement, graph parsing, structural changes with type checking in consideration, Small bug fix related to type changing with tuples.
    Type checking for functions. Overall small logic improvement like lambda map function so that we don't have selfless functions (approximately staticmethod equivalent functions) for mapping. Now includes graph parsing and overall better structural changes with type checking in consideration. Small bug fix related to type changing with tuples, they need to contain an extra index to keep their tuple type, otherwise single string tuple values get  converted to strings and this has been notified in the code.

--- events/importer/juponew.py
+++ events/importer/juponew.py
@@ -17,7 +17,6 @@ import logging
 from os import mkdir
 from os.path import abspath, join, dirname, exists, basename, splitext
 
-
 # Django:
 from django_orghierarchy.models import Organization
 from django_orghierarchy.models import OrganizationClass
@@ -26,6 +25,10 @@ from events.models import Keyword, KeywordLabel, DataSource, BaseModel, Language
 
 # Importer specific:
 from .base import Importer, register_importer
+
+# Type checking:
+import typing
+from typing import Any, Tuple
 
 # Setup Logging:
 if not exists(join(dirname(__file__), 'logs')):
@@ -48,13 +51,104 @@ logger.addHandler(
     logFile
 )
 
-# Create a new importer class.
+
+def fetch_graph() -> dict:
+    # Generate graph base, and parse the file.
+    try:
+        graph = rdflib.Graph()
+        graph.parse('http://finto.fi/rest/v1/jupo/data')
+    except Exception as e:
+        logger.error("Error while fetching JUPO Graph file: %s" % e)
+    # LICENSE http://creativecommons.org/licenses/by/3.0/: https://finto.fi/jupo/fi/
+    logger.info("#2: Graph parsing finished in: %s. Preparing graph..." %
+                time.process_time())
+    return graph
+
+
 @register_importer
 class JupoImporter(Importer):
     # Importer class dependant attributes:
-    name = "juponew" # Command calling name.
-    supported_languages = ['fi', 'sv', 'en'] # Base file requirement.
+    name = "juponew"  # Command calling name.
+    supported_languages = ['fi', 'sv', 'en']  # Base file requirement.
 
+    def iterator(self: 'events.importer.juponew.JupoImporter', data: dict, key: str, query: Any, obj_model: tuple, attr_map: tuple) -> None:
+        # Main class data logic. Create DB objects & set class attributes.
+        for idx, sub_key in enumerate(data[key]):
+            try:
+                q_obj = query()
+                for count, attr in enumerate(obj_model):
+                    setattr(q_obj, attr, data[key][sub_key][count])
+                q_obj.save()
+                setattr(self, attr_map[idx], query.objects.get(
+                    id=data[key][sub_key][0]))
+                keyfinder = '%s_%s' % (key, sub_key)
+                for t_key in data['funcargs']['terms']:
+                    for sub_t_key in data[t_key]:
+                        if data[t_key][sub_t_key][-1] == keyfinder:
+                            data[t_key][sub_t_key][-1] = getattr(
+                                self, attr_map[idx])
+            except Exception as e:
+                logger.error(e)
+
+    # Setup our class attributes & Add to DB.
+    def setup(self) -> None:
+        # Data mapped by models order:
+        self.data = {
+            # YSO, JUPO and the Public DataSource for Organizations model.
+            'ds': {
+                'yso': ('yso', 'Yleinen suomalainen ontologia', True),
+                'jupo': ('jupo', 'Julkisen hallinnon palveluontologia', True),
+                'org': ('org', 'Ulkoa tuodut organisaatiotiedot', True),
+            },
+            # Public organization class for all instances.
+            'orgclass': {
+                'sanasto': ['org:13', '13', 'Sanasto', BaseModel.now(), 'ds_org'],
+            },
+            # YSO & JUPO organizations for keywords.
+            'org': {
+                'yso': ['yso:1200', '1200', 'YSO', BaseModel.now(), 'org:13', 'ds_yso'],
+                'jupo': ['jupo:1300', '1300', 'JUPO', BaseModel.now(), 'org:13', 'ds_jupo'],
+            },
+            # Attribute name mapping for all due to class related attributes (ex. data_source and organization are necessary).
+            'attr_maps': {
+                'ds': ('data_source', 'data_source_jupo', 'data_source_org'),
+                # Tuples get converted to strings for single values if they don't contain , at the end
+                'orgclass': ('organization_class_13',),
+                'org': ('organization', 'organization_jupo'),
+            },
+            # Models for easy iteration (Selected attributes):
+            'model_maps': {
+                'ds': ('id', 'name', 'user_editable'),
+                'orgclass': ('id', 'origin_id', 'name', 'created_time', 'data_source_id'),
+                'org': ('id', 'origin_id', 'name', 'created_time', 'classification_id', 'data_source_id'),
+            },
+            # Function arguments.
+            'funcargs': {
+                'terms': ('ds', 'orgclass', 'org'),
+                'termobjs': (DataSource, OrganizationClass, Organization)
+            },
+        }
+
+        mapped = list(map(lambda f, fto, mm, atm: [f, fto, self.data['model_maps'][mm], self.data['attr_maps'][atm]],
+                      self.data['funcargs']['terms'], self.data['funcargs']['termobjs'], self.data['model_maps'], self.data['attr_maps']))
+
+        for args in mapped:
+            self.iterator(
+                data=self.data, key=args[0], query=args[1], obj_model=args[2], attr_map=args[3])
+
+        logger.info(
+            "#1: Setup finished in: %s. Fetching JUPO turtle graph file..." % time.process_time())
+
+        self.handler()
+
+    def handler(self: 'events.importer.juponew.JupoImporter') -> None:
+        # Handler function for passing the graph between functions. More organized at the cost of more function calls.
+        logger.info("Here but not done yet.")
+        self.graph = fetch_graph()
+
+    # CODE DOCUMENTATION:
+
+    # Class function: iterator()
     '''
         Rundown of what the iterator function does:
 
@@ -74,22 +168,7 @@ class JupoImporter(Importer):
         and assigning the newly created DB object from self.
     '''
 
-    def iterator(self, data, key, query, obj_model, attr_map) -> None:
-        for idx, sub_key in enumerate(data[key]):
-            try:
-                q_obj = query()
-                for count, attr in enumerate(obj_model):
-                    setattr(q_obj, attr, data[key][sub_key][count])
-                q_obj.save()
-                setattr(self, attr_map[idx], query.objects.get(id=data[key][sub_key][0]))
-                keyfinder = '%s_%s' % (key, sub_key)
-                for t_key in data['funcargs']['terms']:
-                    for sub_t_key in data[t_key]:
-                        if data[t_key][sub_t_key][-1] == keyfinder:
-                            data[t_key][sub_t_key][-1] = getattr(self, attr_map[idx])
-            except Exception as e:
-                logger.error(e)
-
+    # Class function: setup()
     '''
         Rundown of how setup works:
 
@@ -170,46 +249,16 @@ class JupoImporter(Importer):
 
         And then make sure to add your attribute mapping into 'attr_maps' key:
         'orgclass': ('organization_class_13', 'organization_class_14'),
-    '''
-    # Setup our class attributes & Add to DB.
-    def setup(self) -> None:
-        # Data mapped by models order:
-        data = {
-            'ds': {
-                'yso': ('yso', 'Yleinen suomalainen ontologia', True),
-                'jupo': ('jupo', 'Julkisen hallinnon palveluontologia', True),
-                'org': ('org', 'Ulkoa tuodut organisaatiotiedot', True),
-            },
-            'orgclass': {
-                'sanasto': ['org:13', '13', 'Sanasto', BaseModel.now(), 'ds_org'],
-            },
-            'org': {
-                'yso': ['yso:1200', '1200', 'YSO', BaseModel.now(), 'org:13', 'ds_yso'],
-                'jupo': ['jupo:1300', '1300', 'JUPO', BaseModel.now(), 'org:13', 'ds_jupo'],
-            },
-            # Attribute name mapping for all due to class related attributes (ex. data_source and organization are necessary).
-            'attr_maps': {
-                'ds': ('data_source', 'data_source_jupo', 'data_source_org'),
-                'orgclass': ('organization_class_13'),
-                'org': ('organization', 'organization_jupo'),
-            },
-            # Models for easy iteration (Selected attributes):
-            'model_maps': {
-                'ds': ('id', 'name', 'user_editable'),
-                'orgclass': ('id', 'origin_id', 'name', 'created_time', 'data_source_id'),
-                'org': ('id', 'origin_id', 'name', 'created_time', 'classification_id', 'data_source_id'),
-            },
-            # Function arguments.
-            'funcargs': {
-                'terms': ('ds', 'orgclass', 'org'),
-                'termobjs': (DataSource, OrganizationClass, Organization)
-            },
-        }
 
-        def mkargs(f, fto, mm, atm):
-            return f, fto, data['model_maps'][mm], data['attr_maps'][atm]
+
+        ------- Add a completely new model -------
         
-        for args in list(map(mkargs, data['funcargs']['terms'], data['funcargs']['termobjs'], data['model_maps'], data['attr_maps'])):
-            self.iterator(data=data, key=args[0], query=args[1], obj_model=args[2], attr_map=args[3])
-
-        logger.info("Done. %s" % time.process_time())
+        Right now we have 'ds' for DataSource, 'orgclass' for OrganizationClass and
+        'org' for Organization. But you can add more underneath 'org', it has to be
+        in order, and then map the given values for them to your liking, 
+        and they are mapped in order by how you've added them to the 'model_maps'.
+        I decided against using dictionaries within each Model because it becomes
+        less organized, in this case it's nicer to keep them separate, one, for
+        easy visualizaion and generally avoiding the data dictionary
+        become a three level multilevel dictionary.
+    '''
