2d615f7e57cb9f1da8c34acf6c7031dccbe12b82
Anthon98 <anthon.kallankari@gmail.com>
2021-11-05 12:35:46 +0200

    Removed test_event_put "cannot_update_an_event_without_a_description"
    Removed the test "cannot_update_an_event_without_a_description" because the requirement for the feature itself has changed.

--- events/tests/test_event_put.py
+++ events/tests/test_event_put.py
@@ -100,27 +100,6 @@ def test__cannot_update_an_event_without_a_short_description(api_client, minimal
 
 
 @pytest.mark.django_db
-def test__cannot_update_an_event_without_a_description(api_client, minimal_event_dict, user):
-
-    # create an event
-    api_client.force_authenticate(user=user)
-    response = create_with_post(api_client, minimal_event_dict)
-    assert_event_data_is_equal(minimal_event_dict, response.data)
-    data2 = response.data
-
-    # delete name
-    data2['description'] = {'fi': None}
-    event_id = data2.pop('@id')
-    response2 = update_with_put(api_client, event_id, data2)
-    assert response2.status_code == 400
-    assert 'description' in response2.data
-    data2.pop('description')
-    response2 = update_with_put(api_client, event_id, data2)
-    assert response2.status_code == 400
-    assert 'description' in response2.data
-
-
-@pytest.mark.django_db
 def test__keyword_n_events_updated(api_client, minimal_event_dict, user, data_source, make_keyword_id):
 
     # create an event
